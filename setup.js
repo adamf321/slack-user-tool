e70082204344d3780fbd55a1667d4b44135eb22b
adamf321 <adam.fenton@gmail.com>
2022-08-13 14:34:28 -0500

    Remove dot from function name (Lambda doesn't like it)

--- setup.js
+++ setup.js
@@ -4,6 +4,7 @@ const { SendMessageCommand, SQSClient } = require("@aws-sdk/client-sqs");
 
 const SLACK_URL = "https://slack.com/api/users.list?limit=200"
 const SLACK_TOKEN = "xoxb-3921443771269-3949371053696-pnB5Gdfv9lUzOkUZ9ZReDKBk";
+const AWS_REGION = "us-east-1";
 const SQS_URL = "https://sqs.us-east-1.amazonaws.com/367114526435/aws-node-project-prod-user-event";
 const DB_HOST = "workos.cclfxume2xkm.us-east-1.rds.amazonaws.com";
 const DB_NAME = "slack";
@@ -28,8 +29,6 @@ const DB_PASSWORD = "12345678";
 
   console.log("Deleted all records from the users table");
 
-  const sqs = new SQSClient({ region: "us-east-1" });
-
   const res = await axios.get(SLACK_URL, {
     headers: { Authorization: `Bearer ${SLACK_TOKEN}` }
   });
@@ -39,6 +38,8 @@ const DB_PASSWORD = "12345678";
   if (!data.ok) throw new Error("Something is up with the data");
 
   console.log(`Retrieved ${data.members.length} users`);
+
+  const sqs = new SQSClient({ region: AWS_REGION });
 
   for (user of data.members) {
     const payload = {
